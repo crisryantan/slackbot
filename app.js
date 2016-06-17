64dc8ab949e731a962d7ad49ccdf2980938707f2
Cris Ryan Tan <crisryantan@gmail.com>
2016-06-17 21:08:19 +0800

    chore: add server
    - added package.json
    - added .gitignore

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Hapi   = require( 'hapi' );
+const server = new Hapi.Server();
+const port   = process.env.port || 3000;
+
+// connect to port
+server.connection( { 'port' : port } );
+
+server.start( function ( err ) {
+	if ( !err ) {
+		console.log( 'Server running at port: ' + port );
+		return;
+	}
+	console.log( 'An error occured' );
+} );
+
