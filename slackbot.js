5fbf6aa6fba654ba24a411d69ff7815e8cfc5cb0
Cris Ryan Tan <crisryantan@gmail.com>
2016-06-18 00:38:10 +0800

    refactor: code organization
    - for slackbot.js

--- slackbot.js
+++ slackbot.js
@@ -19,13 +19,22 @@ function run () {
 		if ( err ) {
 			throw new Error( 'Could not connect to Slack' );
 		}
-		// For reference: https://github.com/howdyai/botkit/blob/master/readme-slack.md#message-received-events
-		controller.hears( [ 'tell a joke' ], 'direct_mention,ambient', function( bot, message ) {
-			JokeApi.getJoke().then( function ( joke ) {
-				bot.reply( message, joke );
-			} );
+		listenToEvents();
+	} );
+}
+
+function listenToEvents () {
+	// For reference: https://github.com/howdyai/botkit/blob/master/readme-slack.md#message-received-events
+	jokeEvent();
+}
+
+function jokeEvent () {
+	controller.hears( [ 'tell a joke' ], 'direct_mention,ambient', function( bot, message ) {
+		JokeApi.getJoke().then( function ( joke ) {
+			bot.reply( message, joke );
 		} );
 	} );
 }
+
 
 module.exports = Slackbot;
